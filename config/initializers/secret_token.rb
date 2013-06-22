57b1764964c3fd5976272ef3ef6f547b0530a399
Vassilis <vas.terzo@gmail.com>
2013-06-22 22:33:19 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chalkids::Application.config.secret_token = '05645607f1b58245d91cd15eb0cf058001a31c115a740fb6e3741d37e3cefdb4e468495af96978aaf038029a504ee97944b955ca91b021db0f59e812ed424521'
